d0b83d483e661b9b850d79ea42cc2ed34d5539eb
chat801aasa <87746529+chat801aasa@users.noreply.github.com>
2021-07-21 00:23:22 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDRFoOEt36BQTfAdX69Z9RB1LbXENgNpXI",
-  authDomain: "chatdwp.firebaseapp.com",
-  projectId: "chatdwp",
-  storageBucket: "chatdwp.appspot.com",
-  messagingSenderId: "596054803702",
-  appId: "1:596054803702:web:2eb541bf84510a206bd8d7",
-  measurementId: "G-YTKR711Y8Z"
+ apiKey: "AIzaSyDUVlkhS4Vv5CSK7SvFLgRO3iwrS_FsNxE",
+    authDomain: "chat801aasa-6af83.firebaseapp.com",
+    projectId: "chat801aasa-6af83",
+    storageBucket: "chat801aasa-6af83.appspot.com",
+    messagingSenderId: "336066690350",
+    appId: "1:336066690350:web:4cdcb5c39374a9bc3e4ebf",
+    measurementId: "G-KDK7PRM6Q1"
     });
